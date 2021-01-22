d422e0f33f53ded062583a71b888c5628e286b3f
Edison Villarta <edtvillarta@outlook.com>
2021-01-22 22:30:41 +0800

    add sample unit test

---
+++ app/src/test/java/com/esys/mvvmboilerplate/viewmodels/HomeViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.esys.mvvmboilerplate.viewmodels
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.esys.mvvmboilerplate.data.repository.AuthRepository
+import com.esys.mvvmboilerplate.modules.home.HomeViewModel
+import com.esys.mvvmboilerplate.MainCoroutineScopeRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class HomeViewModelTest {
+    @get:Rule
+    val rule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineScopeRule = MainCoroutineScopeRule()
+
+    @Mock
+    private lateinit var authRepository: AuthRepository
+
+    @Mock
+    private lateinit var loadingObserver: Observer<Boolean>
+
+    @Test
+    fun `try loading `() {
+        val viewModel = HomeViewModel(authRepository)
+        coroutineScopeRule.runBlockingTest {
+            viewModel.loading.observeForever(loadingObserver)
+            viewModel.tryShowLoading()
+        }
+
+        verify(loadingObserver).onChanged(true) //Show Loading
+        verify(loadingObserver).onChanged(false) //Dismiss Loading
+    }
+}
